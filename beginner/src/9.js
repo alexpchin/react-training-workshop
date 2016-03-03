a9d9a184c2af0c415775dac61f8720de4bba93af
Jack Franklin <jack@jackfranklin.net>
2016-03-03 16:22:17 +0000

    Add the beginner lessons and content

---
+++ beginner/src/9.js
@@ -0,0 +1,47 @@
+// NOTE: in a real world app this input would be a controlled input
+// but I've left it as is for now to keep the example smaller
+var TodoForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onCreate(this.refs.todoInput.value);
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref="todoInput" />
+        <button type="submit">Create</button>
+      </form>
+    )
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return { todos: ['Run workshop'] }
+  },
+
+  onCreate: function(todo) {
+    this.setState({
+      todos: this.state.todos.concat([todo])
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h1>Todos</h1>
+        <TodoForm onCreate={this.onCreate} />
+        { this.state.todos.map(function(todo) {
+            return <p key={todo}>{todo}</p>
+          })
+        }
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
