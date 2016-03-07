9a2574b14c52025a6fad908bc15b3242195facce
Jack Franklin <jack@jackfranklin.net>
2016-03-07 14:21:17 -0800

    Solutions to exercise 6

--- middle/6-react-router-intro/src/app.js
+++ middle/6-react-router-intro/src/app.js
@@ -4,12 +4,28 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
 var hashHistory = ReactRouter.hashHistory;
+
 
 var App = React.createClass({
   render: function() {
     return (
-      <p>Hello World</p>
+      <div>
+        <Link to='/about'>About Page</Link>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+});
+
+var About = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Link to='/'>Home Page</Link>
+        <p>This is the about page</p>
+      </div>
     );
   }
 });
@@ -17,6 +33,7 @@ var App = React.createClass({
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/about" component={About} />
   </Router>,
   document.getElementById('app')
 )
