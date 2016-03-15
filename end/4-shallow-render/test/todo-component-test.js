d0ca39a3d97c2aaf8631b36f863f75fc11786d26
Jack Franklin <jack@jackfranklin.net>
2016-03-15 14:56:01 +0000

    End/4 exercises

--- end/4-shallow-render/test/todo-component-test.js
+++ end/4-shallow-render/test/todo-component-test.js
@@ -21,5 +21,20 @@ test('Todo component', function(t) {
       t.plan(1);
       t.equal(result.props.children[0].props.children, 'Buy Milk');
     });
+
+    t.test('it does not include the done-todo class', function(t) {
+      t.plan(1);
+      t.equal(result.props.className.indexOf('done-todo'), -1);
+    });
+  });
+
+  t.test('rendering a done tweet', function(t) {
+    var todo = { id: 1, name: 'Buy Milk', done: true };
+    var result = shallowRenderTodo(todo);
+
+    t.test('It includes the done-todo class', function(t) {
+      t.plan(1);
+      t.ok(result.props.className.indexOf('done-todo') > -1);
+    });
   });
 });
