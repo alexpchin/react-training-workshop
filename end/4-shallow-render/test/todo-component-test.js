a2715d942a745b3254d5572640d380be1f158161
Jack Franklin <jack@jackfranklin.net>
2016-03-19 14:52:48 +0000

    Prep for me doing the workshop

--- end/4-shallow-render/test/todo-component-test.js
+++ end/4-shallow-render/test/todo-component-test.js
@@ -5,21 +5,9 @@ var test = require('tape');
 var Todo = require('../src/todo');
 
 function shallowRenderTodo(todo) {
-  var fn = function() {};
-
-  const renderer = TestUtils.createRenderer();
-  renderer.render(<Todo todo={todo} deleteTodo={fn} doneChange={fn} />);
-  return renderer.getRenderOutput();
 }
 
 test('Todo component', function(t) {
   t.test('rendering a not-done tweet', function(t) {
-    var todo = { id: 1, name: 'Buy Milk', done: false };
-    var result = shallowRenderTodo(todo);
-
-    t.test('It renders the text of the todo', function(t) {
-      t.plan(1);
-      t.equal(result.props.children[0].props.children, 'Buy Milk');
-    });
   });
 });
