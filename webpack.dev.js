938f2ca97e9242e2a16e3b14a927b65166eb918d
Jack Franklin <jack@jackfranklin.net>
2016-05-31 14:46:55 +0100

    Add ES2015 preset despite the fact that most of the stuff is non ES2015

--- webpack.dev.js
+++ webpack.dev.js
@@ -31,7 +31,7 @@ module.exports = {
       loader: 'babel',
       exclude: /node_modules/,
       query: {
-        presets: ['react'],
+        presets: ['react', 'es2015'],
         cacheDirectory: true
       }
     }]
